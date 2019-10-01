6eb2b80320a3fc474d269c6896bf1ec8995dae0b
Arkoprobho-Pal <arkoprobho.helloworld@gmail.com>
2019-10-01 17:38:23 +0530

    task4 completed , implemented ApplicationContextAware, BeanFactoryAware, BeanNameAware in the Movie class and printed out their results.

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,6 +1,15 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie implements BeanNameAware,BeanFactoryAware,ApplicationContextAware{
+
+
     private Actor actor;
     public Movie(){}
 
@@ -15,5 +24,20 @@ public class Movie {
 
     public void setActor(Actor actor) {
         this.actor = actor;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Going through BeanFactoryAware : "+beanFactory);
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Going through BeanNameAware : "+s);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Going through ApplicationContextAware: "+applicationContext);
     }
 }
