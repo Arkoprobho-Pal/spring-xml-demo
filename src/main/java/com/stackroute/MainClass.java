d63009606ca29727949afa5f466f07fbf7994661
Arkoprobho-Pal <arkoprobho.helloworld@gmail.com>
2019-10-01 17:56:11 +0530

    task5 partially done upto creating BeanLifecycleDemoBean class and define it to beans.xml

--- src/main/java/com/stackroute/MainClass.java
+++ src/main/java/com/stackroute/MainClass.java
@@ -1,4 +1,5 @@
 package com.stackroute;
+import com.stackroute.demo.BeanLifecycleDemoBean;
 import com.stackroute.domain.Movie;
 import org.springframework.beans.factory.support.BeanDefinitionReader;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
@@ -7,18 +8,20 @@ import org.springframework.beans.factory.xml.BeanDefinitionDecorator;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class MainClass {
     public static void main(String[] args) {
 
-        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+        AbstractApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
         Movie movieA=context.getBean("movieA",Movie.class);
         movieA.ActorInfo();
         Movie movieB=context.getBean("movieB",Movie.class);
         movieB.ActorInfo();
-
+        BeanLifecycleDemoBean beanLifecycleDemoBean=context.getBean("beanlifecycledemobean",BeanLifecycleDemoBean.class);
+        context.registerShutdownHook();
 
     }
 }
